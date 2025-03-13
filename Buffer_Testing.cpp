29c1af093b770d1eb5817c11aa327d6ed31d580a
root <root@Starship>
2025-03-12 22:33:51 -0400

    3/12/2025

---
+++ Buffer_Testing.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+
+int main(void){
+    
+    std::cout << "Enter three numbers separated by a space: ";
+
+    int x{}, y{}, z{}; 
+
+    std::cin >> x >> y >> z; // get two numbers and store in variable x and y respectively
+
+    std::cout << "You entered " << x << ", " << y << " and " << z <<  '\n';
+    
+    return 0;
+}
